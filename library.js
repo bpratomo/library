a1b1225e38b76ffca7ee75330083be8685e9cc2b
budi <bpratomo@myrsm.nl>
2021-06-13 16:52:57 +0200

    refactored to use class

--- library.js
+++ library.js
@@ -1,97 +1,172 @@
-// Objects
-let myLibrary = [];
+let DomManager = (() => {
+  function initiate() {
+    setupOverlayToggle();
+    setupSampleEntry();
+  }
 
-function Book(
-  title,
-  author = "unknown",
-  numberOfPages = 0,
-  readStatus = "not read"
-) {
-  this.title = title;
-  this.author = author;
-  this.numberOfPages = numberOfPages;
-  this.readStatus = readStatus;
+  function toggleOverlay() {
+    let overlay = document.querySelector(".overlay");
+    let isShown = overlay.classList.contains("show");
+    if (isShown) {
+      overlay.classList.remove("show");
+      overlay.classList.add("noshow");
+    } else {
+      overlay.classList.add("show");
+      overlay.classList.remove("noshow");
+    }
+  }
 
-  this.info = () => {
-    return `${this.title} by ${this.author}, ${numberOfPages} pages, ${readStatus} `;
+  function setupOverlayToggle() {
+    //Add event to toggle overlay one and off
+    let addButton = document.querySelector(".button__add");
+    let overlay = document.querySelector(".overlay");
+
+    addButton.addEventListener("click", toggleOverlay);
+
+    overlay.addEventListener("click", (e) => {
+      console.log(e.target)
+      if (
+        e.target.classList.contains("overlay") ||
+        e.target.classList.contains("button__close")
+      ) {
+        toggleOverlay();
+        BookForm.clear();
+      }
+    });
+  }
+
+  function setupSampleEntry() {
+    BookLibrary.add({
+      title: "Harry Potter and The Cursed Child",
+      author: "J.K. Rowling",
+      numberOfPages: 245,
+      readStatus: "unread",
+    });
+  }
+
+  return {
+    initiate,
+    setupOverlayToggle,
+    setupSampleEntry,
   };
+})();
 
-  this.toDiv = () => {
+class Book {
+  constructor({ title, author, numberOfPages, readStatus }) {
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.readStatus = readStatus;
+  }
+
+  toDiv() {
     let div = document.createElement("div");
     div.innerHTML = `
     <div class="bookCover">
-    <img src="https://images-na.ssl-images-amazon.com/images/I/61+NT0zxLdL._SY344_BO1,204,203,200_.jpg" />   
+        <img src="https://images-na.ssl-images-amazon.com/images/I/61+NT0zxLdL._SY344_BO1,204,203,200_.jpg" />   
     </div>
 
-    <div class="bookData ${this.readStatus}">
-    <h4>${this.title}</h4>
-    <p>
-    By: ${this.author} <br>
-    Number of pages: ${this.numberOfPages}    
-    </p>
-    <div
-
-
-
-    </div>
+      <div class="bookData ${this.readStatus}">
+          <h4>${this.title}</h4>
+          <p>
+            By: ${this.author} <br>
+            Number of pages: ${this.numberOfPages}    
+          </p>
+      </div>
     `;
 
-    div.setAttribute("data-title", this.title);
-    div.setAttribute("data-author", this.author);
-    div.setAttribute("data-pageNumber", this.numberOfPages);
-    div.setAttribute("data-readStatus", this.readStatus);
+    div.setAttribute("data-bookId", this.title);
     div.setAttribute("class", "book");
 
     return div;
-  };
-}
-
-function addBookToLibrary(title, author, numberOfPages, readStatus) {
-  let library = document.getElementsByClassName("library")[0];
-  let bookToPush = new Book(title, author, numberOfPages, readStatus);
-  let divToCreate = bookToPush.toDiv();
-  myLibrary.push(bookToPush);
-  library.appendChild(divToCreate);
-  console.log(myLibrary);
-}
-
-let addButton = document.querySelector(".button__add");
-addButton.addEventListener("click", (e) => {
-  document.querySelector(".overlay").style.display = "flex";
-});
-
-
-
-let overlay = document.querySelector(".overlay");
-overlay.addEventListener("click", (e) => {
-  if (
-    e.target.classList.contains("overlay") ||
-    e.target.classList.contains("button__close")
-  ) {
-    overlay.style.display = "none";
   }
-});
-
-function addBookFromForm() {
-  let tempBook = []
-  let inputs = document.querySelectorAll("input")
-  inputs.forEach((element) => {
-    tempBook.push(element.value)
-    console.log(tempBook)
-  })
-
-  let [title, author, numberOfPages, readStatus] = tempBook
-  addBookToLibrary(title,author,numberOfPages,readStatus)
-  
 }
 
+let BookLibrary = (() => {
+  let myLibrary = [];
+  let library = document.getElementsByClassName("library")[0];
 
+  function load() {}
 
+  function add(bookData) {
+    let tempBook = new Book(bookData);
+    let divToCreate = tempBook.toDiv();
+    myLibrary.push(tempBook);
+    library.appendChild(divToCreate);
+    console.log(myLibrary);
+  }
 
+  function remove() {}
 
-addBookToLibrary(
-  "Harry Potter and The Cursed Child",
-  "J.K. Rowling",
-  245,
-  "unread"
-);
+  return { load, add, remove };
+})();
+
+let BookForm = (() => {
+  //class library
+  let validateText = (value) => {
+    true;
+  };
+  let validateNumber = (value) => {
+    true;
+  };
+  let validateStatus = (value) => {
+    true;
+  };
+
+  let validationFunctions = {
+    title: validateText,
+    author: validateText,
+    pageNumber: validateNumber,
+    readStatus: validateStatus,
+  };
+
+  let inputs = document.querySelectorAll("input");
+  let incorrectCount = 0;
+
+  //class methods
+
+  function validate() {
+    incorrectCount = 0;
+
+    inputs.forEach((element) => {
+      let isValid = validationFunctions[element.getAttribute("id")](
+        element.value
+      );
+      incorrectCount = isValid ? incorrectCount : incorrectCount + 1;
+    });
+
+    return incorrectCount === 0 ? true : false;
+  }
+
+  function flagInvalid() {
+    alert("book not valid!");
+  }
+
+  function retrieve() {
+    let values = [];
+    inputs.forEach((element) => values.push(element.value));
+    return values;
+  }
+
+  function submit() {
+    let isValid = validate();
+    if (isValid) {
+      let bookData = retrieve();
+      BookLibrary.add(bookData);
+    } else {
+      flagInvalid();
+    }
+  }
+
+  function clear() {
+    inputs.forEach((element) => {
+      element.value = "";
+    });
+  }
+
+  return { submit, clear };
+})();
+
+window.onload = function () {
+  DomManager.initiate();
+};
